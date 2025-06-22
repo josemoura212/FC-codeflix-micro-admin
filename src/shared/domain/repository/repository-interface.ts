28bf02ae17fafa974029b50157bf0ae2122a8692
José Augusto <josemoura212@gmail.com>
2025-06-22 20:45:36 +0000

    ✨ feat(repository): add ISearchableRepository interface and SearchParams/SearchResult classes

--- src/shared/domain/repository/repository-interface.ts
+++ src/shared/domain/repository/repository-interface.ts
@@ -1,6 +1,8 @@
 import { isNumberString } from "class-validator";
 import { Entity } from "../entity";
 import { ValueObject } from "../value-object";
+import { SearchParams } from "./search-params";
+import { SearchResult } from "./search-result";
 
 export interface IRepository<E extends Entity, EntityId extends ValueObject> {
     insert(entity: E): Promise<void>;
@@ -12,4 +14,14 @@ export interface IRepository<E extends Entity, EntityId extends ValueObject> {
     findAll(): Promise<E[]>;
 
     getEntity(): new (...args: any[]) => E;
+}
+
+export interface ISearchableRepository<
+    E extends Entity,
+    EntityId extends ValueObject,
+    SearchInput = SearchParams,
+    SearchOutput = SearchResult
+> extends IRepository<E, EntityId> {
+    sortableFields: string[];
+    search(props: SearchInput): Promise<SearchOutput>;
 }
