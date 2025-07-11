9394ca08b0b06c0342a08e350df51ae73882c6b9
José Augusto <josemoura212@gmail.com>
2025-07-11 22:28:54 +0000

    ✨ feat(tests): refactor integration tests to use setupSequelize helper for database setup

--- src/category/infra/db/sequelize/__test__/category.model.int-spec.ts
+++ src/category/infra/db/sequelize/__test__/category.model.int-spec.ts
@@ -1,21 +1,10 @@
-import { DataType, Sequelize } from "sequelize-typescript";
+import { DataType } from "sequelize-typescript";
 import { CategoryModel } from "../category.model";
-import { Config } from "../../../../../shared/infra/config";
+import { setupSequelize } from "../../../../../shared/infra/testing/helpers";
 
 describe("CategoryModel Integration Tests", () => {
 
-    let sequelize;
-
-    beforeEach(async () => {
-        console.log(Config.db());
-        sequelize = new Sequelize({
-            dialect: "sqlite",
-            storage: ":memory:",
-            models: [CategoryModel],
-            logging: false
-        });
-        await sequelize.sync({ force: true });
-    });
+    setupSequelize({ models: [CategoryModel] });
 
     test("mapping props", () => {
         const attributesMap = CategoryModel.getAttributes();
