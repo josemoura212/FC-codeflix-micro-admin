7b74645fd65e64ef904df783191c006fe68dd855
José Augusto <josemoura212@gmail.com>
2025-07-20 21:24:00 +0000

    ✨ feat(categories): adicionar suporte ao Sequelize para gerenciamento de categorias

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -9,9 +9,12 @@ import {
 } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { CategorySequelizeRepository } from '@core/category/infra/db/sequelize/category-sequelize.repository';
 
 @Controller('categories')
 export class CategoriesController {
+  constructor(private categoryRepo: CategorySequelizeRepository) {}
+
   @Post()
   create(@Body() createCategoryDto: CreateCategoryDto) {}
 
