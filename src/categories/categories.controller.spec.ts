884d7d76af8218e06d45997349e39833613372e1
José Augusto <josemoura212@gmail.com>
2025-07-20 21:37:27 +0000

    ✨ feat(database): criar módulo de banco de dados com configuração do Sequelize

--- src/categories/categories.controller.spec.ts
+++ src/categories/categories.controller.spec.ts
@@ -4,31 +4,15 @@ import { CategorySequelizeRepository } from '@core/category/infra/db/sequelize/c
 import { CategoryModel } from '@core/category/infra/db/sequelize/category.model';
 import { getModelToken } from '@nestjs/sequelize/dist/common';
 import { SequelizeModule } from '@nestjs/sequelize/dist/sequelize.module';
+import { DatabaseModule } from 'src/database/database.module';
+import { CategoriesModule } from './categories.module';
 
 describe('CategoriesController', () => {
   let controller: CategoriesController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [
-        SequelizeModule.forRoot({
-          dialect: 'sqlite',
-          host: ':memory:',
-          logging: false,
-          models: [CategoryModel],
-        }),
-        SequelizeModule.forFeature([CategoryModel]),
-      ],
-      controllers: [CategoriesController],
-      providers: [
-        {
-          provide: CategorySequelizeRepository,
-          useFactory: (categoryModel: typeof CategoryModel) =>
-            new CategorySequelizeRepository(categoryModel),
-          inject: [getModelToken(CategoryModel)],
-
-        },
-      ],
+      imports: [DatabaseModule, CategoriesModule],
     }).compile();
 
     controller = module.get<CategoriesController>(CategoriesController);
