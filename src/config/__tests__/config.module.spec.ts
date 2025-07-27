523aa2be88d28046c95d256179491550125ac9df
José Augusto <josemoura212@gmail.com>
2025-07-27 11:37:57 +0000

    ✨ feat(database): adicionar testes para conexões SQLite e MySQL no módulo de banco de dados

--- src/config/__tests__/config.module.spec.ts
+++ src/config/__tests__/config.module.spec.ts
@@ -4,7 +4,8 @@ import { Test } from '@nestjs/testing';
 import { join } from 'path';
 
 function expectValidate(schema: Joi.Schema, value: any) {
-  return expect(schema.validate(value, { abortEarly: false }).error.message);
+  const error = schema.validate(value, { abortEarly: false }).error;
+  return expect(error ? error.message : undefined);
 }
 
 describe('Schema Unit Tests', () => {
