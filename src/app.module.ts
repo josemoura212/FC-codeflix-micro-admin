038bed58c9b82a70e6e0059e2ec031a56ee9541d
José Augusto <josemoura212@gmail.com>
2025-07-27 12:02:24 +0000

    ✨ feat: reestruturar módulos e remover código não utilizado

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppService } from './app.service';
 import { ConfigModule } from './config/config.module';
-import { AppController } from './app.controller';
 import { DatabaseModule } from './database/database.module';
-import { CategoriesModule } from './categories/categories.module';
+import { CategoriesModule } from './nest-modules/categories-module/categories.module';
 
 @Module({
   imports: [ConfigModule.forRoot(), CategoriesModule, DatabaseModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
