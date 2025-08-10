867fb7ada6e93c1a2304c0de8dc43e59c83a4eb0
José Augusto <josemoura212@gmail.com>
2025-08-10 12:47:42 +0000

    ✨ feat(migrator): adicionar suporte ao umzug para gerenciamento de migrações

---
+++ src/core/shared/infra/db/sequelize/migrator.ts
@@ -0,0 +1,23 @@
+import { join } from 'path';
+import { Sequelize } from 'sequelize';
+import { SequelizeStorage } from 'umzug';
+import { UmzugOptions } from 'umzug/lib/types';
+import { Umzug } from 'umzug/lib/umzug';
+
+export function migrator(sequelize: Sequelize, options: Partial<UmzugOptions>) {
+  return new Umzug({
+    migrations: {
+      glob: [
+        '*/infra/db/sequelize/migrations/*.{js,ts}',
+        {
+          cwd: join(__dirname, '..', '..', '..', '..'),
+          ignore: ['**/*.d.ts', '**/index.ts', '**/index.js'],
+        },
+      ],
+    },
+    context: sequelize,
+    storage: new SequelizeStorage({ sequelize }),
+    logger: console,
+    ...(options || {}),
+  });
+}
